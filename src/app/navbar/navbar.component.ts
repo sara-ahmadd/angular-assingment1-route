f80db9efb2b567817ee26b94473eeb7ae29ffab0
Sara Ahmad <saraahmad2060039@gmail.com>
2024-02-14 14:28:19 +0200

    navbar modifications

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  windowScroll: boolean = false;
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(): void {
+    let scrollPosition = window.scrollY;
+
+    if (scrollPosition > 10) {
+      this.windowScroll = true;
+    } else {
+      this.windowScroll = false;
+    }
+  }
+}
